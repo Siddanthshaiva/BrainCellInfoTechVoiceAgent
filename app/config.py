4ec7a7e6f09a054b6ba9e1f6301419eb09499efc
Siddanth <siddanth.shaiva@gmail.com>
2025-06-30 15:08:36 +0530

    initial commit

---
+++ app/config.py
@@ -0,0 +1,13 @@
+
+EMAIL_ADDRESS = 'notifier.tester12@gmail.com'
+EMAIL_PASSWORD = 'kery xsiu eouv kefo'
+ATTACHMENT_PATH = '/home/siddanth/VoiceGenie/brochure.pdf'
+FIREBASE_ENDPOINT = "https://updatecallanalysis-4v4wa6c35a-el.a.run.app"
+END_OF_CALL_REPORT = 'end-of-call-report'
+CALL_STATUS_ENDED = 'ended'
+CALL_END_REASON = 'customer-did-not-answer'
+CALL_STATUS_FAILED = 'Failed'
+FIREBASE_ACTION = 'create'
+CALL_STATUS_COMPLETED = 'completed'
+FAILED_CALL_SUMMARY = 'Ended without answer, call coud not be initiated or call was not answered by the customer.'
+EMAIL_SUBJECT = 'Your Requested Brochure'
---
+++ app/main.py
@@ -0,0 +1,98 @@
+from fastapi import FastAPI, Request, HTTPException
+import json
+import httpx
+from datetime import timezone, datetime
+import smtplib
+from email.message import EmailMessage
+from pydantic import BaseModel, EmailStr
+from app.config import FAILED_CALL_SUMMARY,CALL_STATUS_ENDED,END_OF_CALL_REPORT, EMAIL_SUBJECT,CALL_STATUS_COMPLETED,FIREBASE_ACTION,EMAIL_ADDRESS, EMAIL_PASSWORD, ATTACHMENT_PATH,CALL_STATUS_FAILED, FIREBASE_ENDPOINT
+import os
+app = FastAPI()
+# EMAIL_ADDRESS = 'notifier.tester12@gmail.com'
+# EMAIL_PASSWORD = 'kery xsiu eouv kefo'
+# ATTACHMENT_PATH = '/home/siddanth/VoiceGenie/brochure.pdf'
+# FIREBASE_ENDPOINT = "https://updatecallanalysis-4v4wa6c35a-el.a.run.app"
+
+
+def send_brochure_email(recipient: str):
+    if not recipient:
+        raise ValueError("Recipient email is empty.")
+
+    if not os.path.exists(ATTACHMENT_PATH):
+        raise FileNotFoundError("brochure.pdf not found.")
+
+    msg = EmailMessage()
+    msg['Subject'] = EMAIL_SUBJECT
+    msg['From'] = EMAIL_ADDRESS
+    msg['To'] = recipient
+    msg.set_content("Hi there,\n\nPlease find attached the brochure you requested.\n\nBest regards,\nNotifier Team")
+
+    with open(ATTACHMENT_PATH, 'rb') as f:
+        file_data = f.read()
+        file_name = os.path.basename(ATTACHMENT_PATH)
+        msg.add_attachment(file_data, maintype='application', subtype='pdf', filename=file_name)
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        smtp.send_message(msg)
+
+class EmailRequest(BaseModel):
+    email: EmailStr
+
+@app.post("/send-brochure")
+async def send_brochure(request: EmailRequest):
+    try:
+        send_brochure_email(request.email)
+        return {"message": f"Brochure sent to {request.email}"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+# Webhook endpoint for VAPI call events
+@app.post("/vapi-webhook")
+async def vapi_webhook(req: Request):
+    payload = await req.json()
+    print("payload",payload)
+    if payload['message']['type'] == END_OF_CALL_REPORT:
+        if payload['message']['call']['status'] == CALL_STATUS_ENDED and payload['message']['endedReason']== 'customer-did-not-answer' and not payload['message']['analysis'] and not payload['message']['artifact']:
+            print("call ended")
+            firebase_data = {
+            "action": FIREBASE_ACTION,
+            "data": {
+                "leadId" : payload['message']['call']['id'],
+                "callStatus": CALL_STATUS_FAILED,   
+                "startedAt": payload['message']['call']['startedAt'],
+                "endedAt": payload['message']['call']['endedAt'],
+                "customerPhone": payload['message']['call']['customer']['number'],           
+                "callAnalysis": {
+                    "summary": FAILED_CALL_SUMMARY,
+                    "successEvaluation": None
+                }
+            }
+        }
+            async with httpx.AsyncClient() as client:
+                response = await client.post(FIREBASE_ENDPOINT, json=firebase_data)
+                print("Firebase CREATE post call status:", response.status_code)
+                print("Firebase CREATE post call response:", response.json())
+        if payload['message']['call']['status'] != CALL_STATUS_ENDED and payload['message']['analysis'] and payload['message']['artifact']:
+            print("call not ended")
+            firebase_data = {
+            "action": "create",
+            "data": {
+                "leadId" : payload['message']['call']['id'],
+                "callStatus": CALL_STATUS_COMPLETED,   
+                "startedAt": payload['message']['startedAt'],
+                "endedAt": payload['message']['endedAt'],
+                "customerPhone": payload['message']['call']['customer']['number'],           
+                "callAnalysis": {
+                    "summary": payload['message']['analysis']['summary'],
+                    "successEvaluation": payload['message']['analysis']['successEvaluation']
+                }
+            }
+        }
+            async with httpx.AsyncClient() as client:
+                response = await client.post(FIREBASE_ENDPOINT, json=firebase_data)
+                print("Firebase CREATE post call status:", response.status_code)
+                print("Firebase CREATE post call response:", response.json())
+    else:
+        print("status - not end of call report")
